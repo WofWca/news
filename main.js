ead5182fa25a7b7a8acf38252102927d0e46b623
WofWca <wofwca@gmail.com>
2019-03-05 18:43:46 +0800

    Implement CRUD

--- main.js
+++ main.js
@@ -2,24 +2,50 @@
 
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectID;
+const bodyParser = require('koa-bodyparser');
 // const newModel = // TODO
 const PORT = 8000; // TODO
+const MONGO_URL = 'mongodb://localhost:27017/';
+const DB_NAME = 'news_app';
+
+let db;
+let newsCollection;
+const dbClient = new MongoClient(MONGO_URL, { useNewUrlParser: true });
+const dbConnectPromise = dbClient.connect();
 
 const app = new Koa();
 const rootRouter = new KoaRouter();
 const newsRouter = new KoaRouter();
 
-newsRouter.get('/', ctx => {
-  ctx.body = [{ asd: 1 }, { asd: 2 }]; // TODO
-});
-newsRouter.post('/', ctx => {
+app.use(bodyParser());
 
+// TODO auth
+// TODO author id
+// TODO validation?
+// TODO is async+await ok in these?
+newsRouter.get('/', async ctx => {
+  ctx.body = await newsCollection.find({}).toArray();
 });
-newsRouter.put('/:id', ctx => {
-  ctx.body = `Changed ${ctx.params.id}`; // TODO
+newsRouter.post('/', async ctx => {
+  const res = await newsCollection.insertOne(ctx.request.body);
+  ctx.body = res.result;
 });
-newsRouter.delete('/:id', ctx => {
-
+newsRouter.put('/:id', async ctx => {
+  const query = { _id: new ObjectID(ctx.params.id) };
+  const update = {
+    $set: ctx.request.body
+  };
+  const res = await newsCollection.updateOne(query, update);
+  ctx.body = res.result;
+});
+newsRouter.delete('/:id', async ctx => {
+  const res = await newsCollection.deleteOne({
+    _id: new ObjectID(ctx.params.id)
+  });
+  ctx.body = res.result;
 });
 
 rootRouter.use('/api/v1/news', newsRouter.routes(), newsRouter.allowedMethods());
@@ -28,4 +54,8 @@ app
   .use(rootRouter.routes())
   .use(rootRouter.allowedMethods());
 
-app.listen(PORT);
+dbConnectPromise.then(() => { 
+  db = dbClient.db(DB_NAME);
+  newsCollection = db.collection('news');
+  app.listen(PORT);
+});
