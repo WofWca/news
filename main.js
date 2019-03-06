682ac34ccc0332cb330e1b43092275a10b4b9fbd
WofWca <wofwca@gmail.com>
2019-03-06 19:44:53 +0800

    Add `authorId` validation on PUT and POST

--- main.js
+++ main.js
@@ -49,13 +49,34 @@ function adminTokenAuthMiddleware() {
 }
 newsRouter.use(adminTokenAuthMiddleware());
 
+async function getAuthorIdObjByIdStr(authorIdStr, authorsCollection) {
+  let authrorIdObj;
+  try {
+    authrorIdObj = ObjectID(authorIdStr);
+  } catch (err) {
+    throw 'Invalid authorId';
+  }
+  const author = await authorsCollection.findOne({ _id: authrorIdObj });
+  if (author === null) {
+    throw 'No user specified by authorId found';
+  }
+  return authrorIdObj;
+}
 newsRouter.get('/', async ctx => {
   ctx.body = await newsCollection.find({}).toArray();
 });
 newsRouter.post('/', async ctx => {
   const newNews = ctx.request.body;
-  if (!newNews.hasOwnProperty('author')) {
+  if (!newNews.hasOwnProperty('authorId')) {
     newNews.authorId = ctx.state.user._id;
+  } else {
+    // Convert authorId string to authorId object.
+    try {
+      newNews.authorId =
+        await getAuthorIdObjByIdStr(newNews.authorId, usersCollection);
+    } catch (err) {
+      ctx.throw(400, err);
+    }
   }
   const res = await newsCollection.insertOne(newNews);
   ctx.body = res.result;
@@ -65,6 +86,15 @@ newsRouter.put('/:id', async ctx => {
   const update = {
     $set: ctx.request.body
   };
+  if (update.$set.hasOwnProperty('authorId')) {
+    // Convert authorId string to authorId object.
+    try {
+      update.$set.authorId =
+        await getAuthorIdObjByIdStr(update.$set.authorId, usersCollection);
+    } catch (err) {
+      ctx.throw(400, err);
+    }
+  }
   const res = await newsCollection.updateOne(query, update);
   ctx.body = res.result;
 });
